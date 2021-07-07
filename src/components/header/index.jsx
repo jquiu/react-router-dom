d5dda82bfe2b03f691bcccc482ec23dac4db6997
Abdul Florez <abdulflorez1524@gmail.com>
2021-07-06 20:08:44 -0500

    agrego Header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,20 @@
+import { link } from 'react-router-dom';
+import "./styles.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <nav>
+                <Link exact to="/" >
+                    Home
+                </Link>
+                <Link to="/cursos" >
+                    Cursos
+                </Link>
+                <Link to="/contactos" >
+                    Contactos
+                </Link>
+            </nav>
+        </header>
+    )
+}
